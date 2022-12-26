d2bf9232f577b193731d28f74508c08017849e14
Jani cenci <nunicenci@gmail.com>
2022-12-26 15:11:05 -0300

    function

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function comprarCafe(){
+    respuesta = prompt("Hay cafe?");
+    if (respuesta == "si"){
+        alert("ya te lo caliento");
+    }else{
+        alert("ahora preparo");
+    }
+    respuesta = prompt("esta bien de azucar?");
+    if (respuesta == "si"){
+        alert("toma tu cafe tranquilo");
+    }else(
+        alert("entonces ponele un poco mas")
+    )
+    respuesta = prompt("esta bien de azucar?");
+    if (respuesta == "si"){
+        alert("toma tu cafe tranquilo");
+    }
+}
+
+comprarCafe()
+comprarCafe()
